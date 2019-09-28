7fcc15c4f954a097265a2e237cf32037aa5142ba
Eduardo <eoalcantara@gmail.com>
2019-09-28 19:51:17 -0300

    Project API Initialized

---
+++ WideWorldImporters.API.UnitTests/DbContextMocker.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WideWorldImporters.API.Models;
+
+namespace WideWorldImporters.API.UnitTests
+{
+    namespace WideWorldImporters.API.UnitTests
+    {
+        public static class DbContextMocker
+        {
+            public static WideWorldImportersDbContext GetWideWorldImportersDbContext(string dbName)
+            {
+                // Create options for DbContext instance
+                var options = new DbContextOptionsBuilder<WideWorldImportersDbContext>()
+                    .UseInMemoryDatabase(databaseName: dbName)
+                    .Options;
+
+                // Create instance of DbContext
+                var dbContext = new WideWorldImportersDbContext(options);
+
+                // Add entities in memory
+                dbContext.Seed();
+
+                return dbContext;
+            }
+        }
+    }
+}
